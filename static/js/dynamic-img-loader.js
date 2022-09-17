f32c03802051fcc7f3f76f6685dbfe8089bfce24
yakkomajuri <yakko.majuri@gmail.com>
2022-09-17 12:44:04 -0300

    yakko.pics 0.1

--- static/js/dynamic-img-loader.js
+++ static/js/dynamic-img-loader.js
@@ -4,7 +4,7 @@ async function sleep(ms) {
   });
 }
 
-async function loadImageGridDynamically(parentElement, prefix, numberOfImages) {
+async function loadImageGridDynamically(parentElement, prefix, numberOfImages, imageFileNames = []) {
   const cols = [];
   for (let i = 0; i < 3; ++i) {
     const col = document.createElement("div");
@@ -12,12 +12,18 @@ async function loadImageGridDynamically(parentElement, prefix, numberOfImages) {
     cols.push(col);
     parentElement.appendChild(col);
   }
+
   for (let i = 1; i <= numberOfImages; ++i) {
-    const imgSrc = `${prefix}/${i}.jpg`;
-    const img = document.createElement("img");
-    img.loading = 'lazy';
-    img.src = imgSrc;
+    const imageFileName = imageFileNames[i - 1] ?? i
+    const img = createImageElement(`${prefix}/${imageFileName}.jpg`);
     cols[(i - 1) % 3].appendChild(img);
     await sleep(50);
   }
+}
+
+function createImageElement(src) {
+  const img = document.createElement("img");
+  img.loading = 'lazy';
+  img.src = src;
+  return img
 }
