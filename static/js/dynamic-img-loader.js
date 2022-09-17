685410b743e261c729e6c71f33bc8eeb7412d7e7
yakkomajuri <yakko.majuri@gmail.com>
2022-09-17 16:16:41 -0300

    hacky af first implementation of image modal (mobile only)

--- static/js/dynamic-img-loader.js
+++ static/js/dynamic-img-loader.js
@@ -16,6 +16,21 @@ async function loadImageGridDynamically(parentElement, prefix, numberOfImages, i
   for (let i = 1; i <= numberOfImages; ++i) {
     const imageFileName = imageFileNames[i - 1] ?? i
     const img = createImageElement(`${prefix}/${imageFileName}.jpg`);
+
+    img.onclick = () => {
+      const modal = document.getElementById("img-popup-modal");
+      modal.style.display = "block";
+      const clonedImg = img.cloneNode(true)
+      clonedImg.style['max-height'] = '80vh'
+      clonedImg.style['width'] = 'auto'
+      clonedImg.style['height'] = 'auto'
+      clonedImg.style['align'] = 'center'
+      clonedImg.style['display'] = 'block'
+      clonedImg.style['margin'] = 'auto'
+      const modalContent = document.getElementById('modal-content')
+      modalContent.innerHTML = ''
+      modalContent.appendChild(clonedImg)
+    }
     cols[(i - 1) % 3].appendChild(img);
     await sleep(50);
   }
